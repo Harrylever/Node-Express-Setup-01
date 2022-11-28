6e4780ecfd2d87b76eb9a872e8ee3e04c7af9b8b
Harrylever <ukanah15thdean@gmail.com>
2022-11-28 06:57:11 +0100

    Added Husky and Eslint Config

--- src/app.js
+++ src/app.js
@@ -1,9 +1,33 @@
-const express = require('express');
+const express = require('express')
 
-const app = express();
-const PORT = 3001;
+const app = express()
+const PORT = 3001
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 app.listen(PORT, () => {
-	console.log(`Running Express Server on Port ${PORT}`);
+  console.log(`Running Express Server on Port ${PORT}`)
 })
 
+app.get('/', (req, res) => {
+  res.send([
+    {
+      item: 'milk',
+      quantity: 2
+    },
+    {
+      item: 'sugar',
+      quantity: 5
+    },
+    {
+      item: 'mayon',
+      quantity: 2
+    }
+  ])
+})
+
+app.post('/', (req, res) => {
+  console.log(req.body)
+  res.send(200)
+})
