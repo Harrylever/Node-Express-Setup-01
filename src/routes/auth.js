7b7f3939e976f08457a5e932ca38c7a64f4c0253
Harrylever <ukanah15thdean@gmail.com>
2022-12-02 02:19:06 +0100

    Setup Basic Auth with Pass

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,40 +1,33 @@
 const { Router } = require('express')
 const User = require('../database/schema/user')
-const { hashPassword } = require('../utils/helpers')
+const { hashPassword, comparePassword } = require('../utils/helpers')
 
 const router = Router()
 
-router.post('/login', (req, res) => {
-  const { username, password } = req.body
-  if (username && password) {
-    if (req.session.user) {
-      if (req.session.user.username === username) {
-        res.send(req.session.user)
-      } else {
-        req.session.user = {
-          username
-        }
-        res.send(req.session.user.username)
-      }
-    } else {
-      req.session.user = {
-        username
-      }
-      res.send(req.session)
-    }
-  } else res.status(401).send('Error please enter valid details')
+router.post('/login', async (req, res) => {
+  const { email, password } = req.body
+  if (!email || !password) return res.status(400).send('Forbidden')
+  const userDB = await User.findOne({ email })
+  if (!userDB) return res.status(401).send('Unauthorized')
+  const isValid = comparePassword(password, userDB.password)
+  if (isValid) {
+    req.session.user = userDB
+    return res.status(200).send('Successfully Logged in')
+  } else {
+    return res.status(400).send('Error logging in')
+  }
 })
 
 router.post('/register', async (req, res) => {
-  const { username, email } = req.body
+  const { email } = req.body
 
-  const userDB = await User.findOne({ $or: [{ username }, { email }] })
+  const userDB = await User.findOne({ email })
   if (userDB) {
     res.status(400).json({ msg: 'User already exists' })
   } else {
     const password = hashPassword(req.body.password)
     console.log(password)
-    const newUser = await User.create({ username, password, email })
+    const newUser = await User.create({ email, password })
     newUser.save()
     res.status(201).json({ statusMsg: 'User created' })
   }
